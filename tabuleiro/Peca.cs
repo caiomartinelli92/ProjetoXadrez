dd6e3cc3cc65c53c19af86cf310b937bf5e084df
Caio Martinelli <caio.martinelli@gmail.com>
2023-07-04 17:32:21 -0300

    Comecando a implementar o tabuleiro.

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca (Posicao posicao, Tabuleiro tab, Cor cor) 
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.Cor = cor;
+            this.qtdMovimentos = 0;
+        }
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linha {  get; set; }
+        public int colunas { get; set; }
+        public Peca[,] pecas;
+
+        public Tabuleiro (int linhas, int colunas)
+        {
+            this.linha = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
