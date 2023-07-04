01c075f5475f1fb8e6fec30eb171c81c8166a5cd
Caio Martinelli <caio.martinelli@gmail.com>
2023-07-04 17:51:04 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Projeto_Xadrez;
+using tabuleiro;
+
+Tabuleiro tab = new Tabuleiro(8,8);
+
+Tela.imprimirTabuleiro(tab);
--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -2,15 +2,20 @@
 {
     class Tabuleiro
     {
-        public int linha {  get; set; }
+        public int linhas {  get; set; }
         public int colunas { get; set; }
         public Peca[,] pecas;
 
         public Tabuleiro (int linhas, int colunas)
         {
-            this.linha = linhas;
+            this.linhas = linhas;
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca (int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
