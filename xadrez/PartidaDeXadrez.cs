4f2402abdfa3b4b408f095a833d7a368922552a1
Caio Martinelli <caio.martinelli@gmail.com>
2023-07-06 19:10:12 -0300

    Jogada especial promocao

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if(p is Peao)
+            {
+                if(p.Cor == Cor.Branca && destino.linha == 0 || p.Cor == Cor.Preta && destino.linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.Cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -158,8 +174,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //#jogadaespecial en passant
             if (p is Peao && destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2)
